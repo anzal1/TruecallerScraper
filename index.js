16b007dfdc7656d89bb3404140d18e2c4d8de0d9
anzal1 <cofclan145@gmail.com>
2022-12-31 17:42:30 +0530

    change to async approach

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const truecallerjs = require("truecallerjs");
 var countryCode = "IN";
 var installationId =
-  "add_auth_token_here";
+  "add_token_here";
 const excelToJson = require("convert-excel-to-json");
 const result = excelToJson({
   sourceFile: "phone_numbers.xlsx",
@@ -13,27 +13,25 @@ var searchData = result.Sheet1.map(function (item) {
 
 //remove the first element of the array
 searchData.shift();
-const timer = (ms) => new Promise((res) => setTimeout(res, ms));
+// const timer = (ms) => new Promise((res) => setTimeout(res, ms));
 
 async function loadData() {
-  var start = 0;
-  for (var start = 0; start < searchData.length; start += 10) {
-    var phoneNumbers = searchData.slice(start, start + 10);
-    phoneNumbers = phoneNumbers.join(",");
-    const searchResult = truecallerjs.bulkSearch(
-      phoneNumbers,
-      countryCode,
-      installationId
-    );
-    searchResult.then(function (response) {
-      const data = JSON.stringify(response, null, 2);
-      console.log(data);
-    });
+  // var start = 0;
+  // for (var start = 0; start < searchData.length; start += 10) {
+  var phoneNumbers = searchData.slice(0, 178);
+  phoneNumbers = phoneNumbers.join(",");
+  const searchResult = truecallerjs.bulkSearch(
+    phoneNumbers,
+    countryCode,
+    installationId
+  );
+  searchResult.then(function (response) {
+    const data = JSON.stringify(response, null, 2);
+    console.log(data);
+  });
 
-    await timer(3000); // then the created Promise can be awaited
-  }
+  // await timer(3000); // then the created Promise can be awaited
+  // }
 }
-
-// console.log(phoneNumbers);
 
 loadData();
