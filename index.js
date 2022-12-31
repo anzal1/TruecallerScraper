2bfabf8709152bf20fab567987478a8e9974d841
anzal1 <cofclan145@gmail.com>
2022-12-31 15:47:01 +0530

    change to async approach

--- index.js
+++ index.js
@@ -2,11 +2,6 @@ const truecallerjs = require("truecallerjs");
 var countryCode = "IN";
 var installationId =
   "add_auth_token_here";
-//use an excel file of phone numbers and extract the phone numbers
-//use the phone numbers to search for the details
-
-//use the country code and installation id to search for the details
-
 const excelToJson = require("convert-excel-to-json");
 const result = excelToJson({
   sourceFile: "phone_numbers.xlsx",
@@ -18,40 +13,27 @@ var searchData = result.Sheet1.map(function (item) {
 
 //remove the first element of the array
 searchData.shift();
+const timer = (ms) => new Promise((res) => setTimeout(res, ms));
 
-// create an string of phone numbers seperated by comma's using only first 10 items
-// phoneNumbers = phoneNumbers.slice(0, 10);
-// phoneNumbers = phoneNumbers.join(",");
-//now use this in loop to search for the details of the phone numbers 10 at a time
-// var start = 0;
-// var end = 10;
-// for (var i = 0; i < searchData.length; i++) {
-//   var phoneNumbers = searchData.slice(start, end);
-  //   phoneNumbers = phoneNumbers.join(",");
+async function loadData() {
+  var start = 0;
+  for (var start = 0; start < searchData.length; start += 10) {
+    var phoneNumbers = searchData.slice(start, start + 10);
+    phoneNumbers = phoneNumbers.join(",");
+    const searchResult = truecallerjs.bulkSearch(
+      phoneNumbers,
+      countryCode,
+      installationId
+    );
+    searchResult.then(function (response) {
+      const data = JSON.stringify(response, null, 2);
+      console.log(data);
+    });
 
-//   var countryCode = "IN";
-// var installationId = "a1k07--Vgdfyvv_rftf5uuudhuhnkljyvvtfftjuhbuijbhug";
-var phoneNumbers = "+9912345678,+14051234567,+919987654321" // Phone numbers seperated by comma's
-
-const searchResult = truecallerjs.bulkSearch(phoneNumbers,countryCode,installationId)
-searchResult.then(function (response) {
-    const data = JSON.stringify(response, null, 2);
-    console.log(data);
-})
-//   phoneNumbers = "+9912345678,+14051234567,+919987654321";
-//   const searchResult = truecallerjs.bulkSearch(
-//     phoneNumbers,
-//     countryCode,
-//     installationId
-//   );
-//   searchResult.then(function (response) {
-//     const data = JSON.stringify(response, null, 2);
-//     console.log(data);
-//   });
-//   setTimeout(function () {}, 1000);
-
-//   start += 10;
-//   end += 10;
-// }
+    await timer(3000); // then the created Promise can be awaited
+  }
+}
 
 // console.log(phoneNumbers);
+
+loadData();
